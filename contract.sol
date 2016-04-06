7238f3a822f04e3751d61c1d4694525c2496f9f1
Eugene Yurtaev <eugene-2306@ya.ru>
2016-04-06 11:22:38 +0300

    added timestamp parameter

--- contract.sol
+++ contract.sol
@@ -17,16 +17,32 @@ contract WavesPresale {
         numberOfSales = 0;
     }
 
-    function newSale(bytes32 txid, uint amount) {
+    function changeOwner(address newOwner) {
+        if (msg.sender != owner) return;
+
+        owner = newOwner;
+    }
+
+    function newSale(bytes32 txid, uint amount, uint timestamp) {
         if (msg.sender != owner) return;
 
         sales.push(Sale({
                 txid: txid,
                 amount: amount,
-                date: now
+                date: timestamp
             }));
         numberOfSales += 1;
         totalTokens += amount;
+    }
+
+    function () {
+        // This function gets executed if a
+        // transaction with invalid data is sent to
+        // the contract or just ether without data.
+        // We revert the send so that no-one
+        // accidentally loses money when using the
+        // contract.
+        throw;
     }
 
 }
--- wavesPresale.py
+++ wavesPresale.py
@@ -5,16 +5,15 @@ base_address = "0x82e1c8c4c38f5c0767f7c426e235f194010b71e9"
 contract_address = "0xac64a5168144734f0d688D689c730A519844D0c5"
 api = API()
 
-def logSale(bitcoin_tx_id, amount):
+def logSale(bitcoin_tx_id, amount, timestamp):
     if len(bitcoin_tx_id) != 64:
         print('TX Id must contain 64 symbols')
         return
+    timestamp = int(timestamp)
     definition = "newSale(bytes32,uint256)"
     method_id = api.getMethodId(definition)
     amount = abs(int(amount * (10 ** 8)))
-    amount_bytes = amount.to_bytes(32, byteorder='big')
-    amount_string = str(binascii.b2a_hex(amount_bytes), 'ascii')
-    data = "0x" + method_id + bitcoin_tx_id + amount_string
+    data = "0x" + method_id + bitcoin_tx_id + uint_to_bytes_string(amount) + uint_to_bytes_string(timestamp)
 
     gas_price = hex(api.getGasPrice()["result"])
     response = api.send_transaction(base_address, contract_address,
@@ -22,3 +21,9 @@ def logSale(bitcoin_tx_id, amount):
                          value=hex(0), data=data)
     print(str(response))
     return data
+
+
+def uint_to_bytes_string(number):
+    number_bytes = number.to_bytes(32, byteorder='big')
+    return str(binascii.b2a_hex(number_bytes), 'ascii')
+
