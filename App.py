08fdd3d86a129592f7f57340bf8a09200a54b6b7
Alex <avbashlykov@gmail.com>
2016-04-08 15:27:33 +0300

    web page

--- App.py
+++ App.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from API import API
 from configs import BASE_ADDRESS, CONTRACT_ADDRESS
+from datetime import datetime
+import binascii
 
 app = Flask(__name__)
 api = API()
@@ -13,11 +15,26 @@ def info():
 
 @app.route("/get_sale/")
 def get_sale():
-    # TODO: here
-    method = "0x" + api.getMethodId("getSale(uint32)")
-    response = api.getInfo(BASE_ADDRESS, CONTRACT_ADDRESS, method)
-    print(response)
-    return jsonify({"status": "OK", "data": response["data"]})
+    try:
+        id = int(request.args.get('id', -1))
+    except ValueError:
+        return jsonify({"status": "OK", "tx_id": "None", "amount": "None", "date": "None"})
+    if id < 0:
+        return jsonify({"status": "OK", "tx_id": "None", "amount": "None", "date": "None"})
+    num_of_sales_method = "0x" + api.getMethodId("numberOfSales()")
+    num_of_sales = api.getInfo(BASE_ADDRESS, CONTRACT_ADDRESS, num_of_sales_method)["data"]
+    num_of_sales = int(num_of_sales, 0)
+    print(id, num_of_sales)
+    if id >= num_of_sales:
+        return jsonify({"status": "OK", "tx_id": "None", "amount": "None", "date": "None"})
+    tx_id_method = "0x" + api.getMethodId("getSaleTxId(uint32)") + uint_to_bytes_string(id)
+    tx_id = api.getInfo(BASE_ADDRESS, CONTRACT_ADDRESS, tx_id_method)["data"]
+    amount_method = "0x" + api.getMethodId("getSaleAmount(uint32)") + uint_to_bytes_string(id)
+    amount = api.getInfo(BASE_ADDRESS, CONTRACT_ADDRESS, amount_method)["data"]
+    date_method = "0x" + api.getMethodId("getSaleDate(uint32)") + uint_to_bytes_string(id)
+    date = api.getInfo(BASE_ADDRESS, CONTRACT_ADDRESS, date_method)["data"]
+    print(tx_id, amount, date)
+    return jsonify({"status": "OK", "tx_id": tx_id, "amount": int(amount, 0) / 10**8, "date": datetime.fromtimestamp(int(date, 0))})
 
 
 @app.route("/get_info/")
@@ -40,6 +57,11 @@ def get_info():
 @app.route("/styles.css")
 def styles():
     return app.send_static_file("styles.css")
+
+
+def uint_to_bytes_string(number):
+    number_bytes = number.to_bytes(32, byteorder='big')
+    return str(binascii.b2a_hex(number_bytes), 'ascii')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
